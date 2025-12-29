3256f3912c901bf3461955c740c9dc1aa371a5ab
Felix Nana Yaw Yeboah <87368189+felixyeboah@users.noreply.github.com>
2025-12-29 12:58:36 +0000

    fix: rename python package to reevit and bump version to 0.3.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="reevit",
-    version="0.3.0",
+    version="0.3.2",
     description="Reevit Python SDK",
     author="Reevit",
     packages=find_packages(),
